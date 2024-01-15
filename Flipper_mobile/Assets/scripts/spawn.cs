402f77d5511d6e5bf75224376657f0d8f47e3022
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-01-15 12:42:58 +0100

    add bouncer + collision ball

--- Flipper_mobile/Assets/scripts/spawn.cs
+++ Flipper_mobile/Assets/scripts/spawn.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawn : MonoBehaviour
+public class Spawn : MonoBehaviour
 {
-
     public GameObject ball;
+    public float force = 20f;
+
     void Start()
     {
-        GameObject newball = Instantiate(ball, transform.position, Quaternion.identity);
+        GameObject newBall = Instantiate(ball, transform.position, Quaternion.identity);
+
+        Rigidbody2D rigidbodyBall = newBall.GetComponent<Rigidbody2D>();
+
+        if (rigidbodyBall != null)
+        {
+            rigidbodyBall.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        }
+        else
+        {
+            Debug.LogWarning("Rigidbody2D not found on the instantiated ball. Add a Rigidbody2D component to apply force.");
+        }
     }
 }
