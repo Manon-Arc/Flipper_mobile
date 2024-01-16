5c0266bec027cb4d55d926bcbefc6feb3ac2607a
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-01-16 15:50:25 +0100

    fin UI menu + redirect

---
+++ Flipper_mobile/Assets/scripts/Menu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+
+    public void Redirect_Start()
+    {
+        Debug.Log("start");
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
