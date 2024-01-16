21b0c6080548ea5b8e46e9bcd83e3457a22eb471
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-01-16 16:55:36 +0100

    update

--- Flipper_mobile/Assets/scripts/ball_score.cs
+++ Flipper_mobile/Assets/scripts/ball_score.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ball_score : MonoBehaviour
 {
-    public int score = 100;
+    public int score = 10;
 
+    public GameObject test;
     public TMP_Text compteur;
 
     private int[] tag_score = { 5, 10, 20, 30, 40, 50, 60, 70, 80, 150 };
@@ -18,13 +19,18 @@ public class ball_score : MonoBehaviour
     private void Start()
     {
         gameManager = GameObject.FindGameObjectWithTag("gamemanager");
+        test = GameObject.FindGameObjectWithTag("Score");
+        compteur = test.GetComponent<TextMeshPro>();
         if (gameManager != null )
         {
             vie = gameManager.GetComponent<Life>();
         }
         
         compteur.text = "Score : " + score.ToString();
+        Debug.Log("test");
     }
+
+
 
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -42,6 +48,7 @@ public class ball_score : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log(collision.gameObject.tag);
         foreach (int tagScore in tag_score)
         {
             if (collision.gameObject.CompareTag(tagScore.ToString()))
