7bd66c84b1543b85ecbc7e8721f7e938ba81b853
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2024-01-22 11:24:50 +0100

    correction bug

--- Flipper_mobile/Assets/scripts/Menu.cs
+++ Flipper_mobile/Assets/scripts/Menu.cs
@@ -11,7 +11,7 @@ public class Menu : MonoBehaviour
     public void Redirect_Start()
     {
         Debug.Log("start");
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Game");
     }
 
 }
